1b0a35a3b5eac874323fa843349b40106b531b69
Tô Việt Hoàng <totranhieu@gmail.com>
2023-06-27 11:06:17 +0700

    hieuhoa

---
+++ src/main/java/com/example/hieuhoa/HieuhoaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hieuhoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HieuhoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HieuhoaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/hieuhoa/model/Student.java
@@ -0,0 +1,26 @@
+package com.example.hieuhoa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+    private String address;
+    private boolean sex;
+}
