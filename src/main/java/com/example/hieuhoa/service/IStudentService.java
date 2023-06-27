9c52057dcb41cce6985305e76d70bed80353259a
Nguyencongnguyen1812 <ngannt33@wru.vn>
2023-06-27 14:02:01 +0700

    ngannt

--- src/main/java/com/example/hieuhoa/service/IStudentService.java
+++ src/main/java/com/example/hieuhoa/service/IStudentService.java
@@ -1,10 +1,6 @@
 package com.example.hieuhoa.service;
 
 import com.example.hieuhoa.model.Student;
-import com.example.hieuhoa.repository.StudentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.Optional;
 
 public interface IStudentService extends IGenericService<Student,Long> {
 }
