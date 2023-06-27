eb19c4c5fa8609b087476b001440be5789a5a6bf
minh khiet <minhkhiet@MacBook-Pro-cua-minh.local>
2023-06-27 11:43:42 +0700

    add entity

--- src/main/java/com/example/hieuhoa/model/Subject.java
+++ src/main/java/com/example/hieuhoa/model/Subject.java
@@ -1,2 +1,23 @@
-package com.example.hieuhoa.model;public class Subject {
+package com.example.hieuhoa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "subjects")
+public class Subject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Student student;
+    private String nameSub;
+    private float mark;
+
 }
